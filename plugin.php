27c1f402de3b30edec2b955a8476bc0a1b2134f7
mstuder <ms@studer-raimann.ch>
2019-04-09 16:55:38 +0200

    plugin version 5.4

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'dhbwautoloui';
 $version = '0.0.3';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'info@studer-raimann.ch';
